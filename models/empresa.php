2b3153743ef36b63f4e85e0821557d7390c5a5e8
Thiago Goveia <goveiat@gmail.com>
2017-02-03 17:46:22 -0200

    commit inicial: api /empresa

---
+++ models/empresa.php
@@ -0,0 +1,50 @@
+<?php
+
+class Empresa {
+
+    public function __construct(){
+
+    }
+
+    public function qsDados(){
+        return "
+            SELECT c.IDCompany, c.Name, c.Background, c.SEOName, c.Description, c.Email, i.URL as Logo
+            from company as c
+            inner join image as i on i.ID = c.IDCompany and i.Type = 'logo'
+            where  c.Username like :host";
+    }
+
+    public function qsEnderecos($id){
+        return "
+            SELECT Address, Address2, Address3, City, SEOCity, Telephone, MapLat, MapLong, Nickname, State
+            from address
+            where Entity = 'company' and IDEntity = $id ";
+    }
+
+    public function qsPagamentos($id){
+        return "
+            SELECT t.Icon, t.Title, t.IDPaymenttype
+            from paymenttype as t
+            inner join companypaymenttype as c on t.IDPaymenttype = c.IDPaymenttype
+            where c.IDCompany = $id ";
+    }
+
+    public function qsHorarios($id, $horario){
+        return "SELECT * from $horario where IDShop = $id ";
+    }
+
+    public function frmHorario($horario){
+
+        for ($i=1; $i < 8; $i++) {
+            $horas = array();
+            foreach ($horario as $h) {
+                if (strpos($h->Day, (string)$i) !== false) {
+                    $horas[] = array('Opens'=> $h->Opens, 'Closes'=>$h->Closes);
+                }
+            }
+            $ret[$i] = $horas;
+        }
+        return $ret;
+    }
+
+}
---
+++ src/routes.php
@@ -0,0 +1,43 @@
+<?php
+$model = new Empresa();
+$app->get('/', function () {
+    echo "Foo!";
+});
+
+$app->get('/empresa/[{host}]', function ($request, $response, $args) use ($model) {
+    $host = "%".$args['host']."%";
+    $retorno = array();
+
+    $d = $this->db->prepare($model->qsDados());
+    $d->bindParam("host", $host);
+    $d->execute();
+    $empresa = $d->fetchObject();
+
+    if($empresa){
+        $retorno['empresa'] = $empresa;
+        $id = $empresa->IDCompany;
+
+        //Endereços
+        $d = $this->db->prepare($model->qsEnderecos($id));
+        $d->execute();
+        $retorno['enderecos'] = $d->fetchAll();
+
+        //Horario Func
+        $d = $this->db->prepare($model->qsHorarios($id, 'businesshours'));
+        $d->execute();
+        $retorno['h_funcionamento'] = $d->fetchAll();
+
+
+        //Horario Entrega
+        $d = $this->db->prepare($model->qsHorarios($id, 'openinghours'));
+        $d->execute();
+        $retorno['h_entrega'] = $d->fetchAll();
+
+        //Formas de pagamento
+        $d = $this->db->prepare($model->qsPagamentos($id));
+        $d->execute();
+        $retorno['tipo_pagamento'] = $d->fetchAll();
+    }
+
+    return $this->response->withJson($retorno);
+});
